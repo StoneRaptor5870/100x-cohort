5e5d19c213ee488da5b1cb33df816f0a62a58b21
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-16 23:34:30 +0530

    part-1

--- 01-JS/easy/anagram.js
+++ 01-JS/easy/anagram.js
@@ -5,21 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
-    // Remove spaces and convert to lowercase for case-insensitive comparison
-    const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
-    const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
+  // Remove spaces and convert to lowercase for case-insensitive comparison
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
 
-    // If lengths are different, they cannot be anagrams
-    if (cleanStr1.length !== cleanStr2.length) {
-        return false;
-    }
+  // If lengths are different, they cannot be anagrams
+  if (cleanStr1.length !== cleanStr2.length) {
+    return false;
+  }
 
-    // Sort the characters and compare the sorted strings
-    const sortedStr1 = cleanStr1.split('').sort().join('');
-    const sortedStr2 = cleanStr2.split('').sort().join('');
+  // Sort the characters and compare the sorted strings
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
 
-    // If the sorted strings are equal, they are anagrams
-    return sortedStr1 === sortedStr2;
+  // If the sorted strings are equal, they are anagrams
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
