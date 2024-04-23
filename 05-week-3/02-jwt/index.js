663dcdcaecd70a9073485aa948a83beb610d9ec6
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-24 00:18:20 +0530

    jwt done

--- 05-week-3/02-jwt/index.js
+++ 05-week-3/02-jwt/index.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const z = require("zod");
 const jwtPassword = "radiohead";
 function signJwt(username, password) {
-    if (username.includes("@"))
+    if (!username.includes("@"))
         return null;
     if (password.length < 6)
         return null;
@@ -17,14 +17,18 @@ function verifyJwt(token) {
             return true;
     }
     catch (error) {
-        console.log(error);
+        return false;
     }
-    return false;
 }
 function decodeJwt(token) {
     const decoded = jwt.decode(token);
-    if (decoded)
-        return decoded;
-    else
+    if (!decoded)
         return false;
+    return true;
 }
+module.exports = {
+    signJwt,
+    verifyJwt,
+    decodeJwt,
+    jwtPassword,
+};
--- 05-week-3/02-jwt/index.ts
+++ 05-week-3/02-jwt/index.ts
@@ -3,7 +3,7 @@ const z = require("zod");
 const jwtPassword = "radiohead";
 
 function signJwt(username: string, password: string): string | null {
-  if (username.includes("@")) return null;
+  if (!username.includes("@")) return null;
   if (password.length < 6) return null;
   const token = jwt.sign(
     {username: username, password: password},
@@ -12,18 +12,24 @@ function signJwt(username: string, password: string): string | null {
   return token;
 }
 
-function verifyJwt(token: string): boolean {
+function verifyJwt(token: string) {
   try {
     const isValid = jwt.verify(token, jwtPassword);
     if (isValid) return true;
   } catch (error) {
-    console.log(error);
+    return false;
   }
-  return false;
 }
 
-function decodeJwt(token: string): object | boolean {
+function decodeJwt(token: string) {
   const decoded = jwt.decode(token);
-  if (decoded) return decoded;
-  else return false;
+  if (!decoded) return false;
+  return true;
 }
+
+module.exports = {
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
