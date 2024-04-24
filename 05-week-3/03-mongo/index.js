d929f4be9217726280fd0ba706f47642010e241b
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-25 01:59:48 +0530

    user route

--- 05-week-3/03-mongo/index.js
+++ 05-week-3/03-mongo/index.js
@@ -4,11 +4,12 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const adminRouter = require("./routes/admin.js");
+const userRouter = require("./routes/user.js");
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
 app.use("/api/admin", adminRouter);
-// app.use("/api/user", userRoutes);
+app.use("/api/user", userRoutes);
 
 const port = process.env.PORT || 5000;
 
