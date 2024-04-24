16dd24f66b7702f9d798d1d55dc527cd81603701
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-25 00:20:14 +0530

    admin route

--- 05-week-3/03-mongo/index.js
+++ 05-week-3/03-mongo/index.js
@@ -3,19 +3,20 @@ const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin.js");
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
-// app.use("/api/admin", adminRouter);
+app.use("/api/admin", adminRouter);
 // app.use("/api/user", userRoutes);
 
-const PORT = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB).then((data) => {
   console.log("Connected to MongoDB");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
