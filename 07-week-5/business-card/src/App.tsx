247826a0e761f645c51b370e12e09603a43c15b3
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-29 15:48:32 +0530

    interface and card

--- 07-week-5/business-card/src/App.tsx
+++ 07-week-5/business-card/src/App.tsx
@@ -1,3 +1,19 @@
+import { PersonPropsTypes } from "./components/Card.tsx";
+
+const person: PersonPropsTypes[] = [
+  {
+    name: "Nischay",
+    interests: ["coding", "music", "video games"],
+    job: "freelance dev",
+    socials: [
+      {
+        socialName: "LinkedIn",
+        url: "https://www.linkedin.com/",
+      },
+    ],
+  },
+];
+
 function App() {
 
   return (
---
+++ 07-week-5/business-card/src/components/Card.tsx
@@ -0,0 +1,40 @@
+export interface PersonPropsTypes {
+  name: string,
+  job: string;
+  interests: string[];
+  socials: Array<{socialName: string; url: string}>;
+}
+
+export default function Card({
+  name,
+  job,
+  interests,
+  socials,
+}: PersonPropsTypes) {
+  return (
+    <div className="__card py-7 min-w-[350px] px-3 border-2 border-gray-300 w-fit rounded-lg bg-white text-black">
+      <h1 className="text-3xl font-bold my-3">{name}</h1>
+      <p>{job}</p>
+
+      <h1 className="text-xl font-bold">Intrests</h1>
+      <ul>
+        {interests.map((interest) => {
+          return <li>{interest}</li>;
+        })}
+      </ul>
+      <div className="__socials mt-4 flex gap-2">
+        {socials.map((social) => {
+          return (
+            <a
+              className="px-4 py-2 bg-blue-500 rounded text-white"
+              target="_blank"
+              href={social.url}
+            >
+              {social.socialName}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
