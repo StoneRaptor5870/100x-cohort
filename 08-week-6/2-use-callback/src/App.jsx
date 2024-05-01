e5adfdede39656e5b1d418b7b7c1831686081bb7
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-01 23:32:14 +0530

    useCallback 2

--- 08-week-6/2-use-callback/src/App.jsx
+++ 08-week-6/2-use-callback/src/App.jsx
@@ -1,11 +1,13 @@
 import Assignment1 from "./components/Assignment1"
+import Assignment2 from "./components/Assignment2"
 
 function App() {
 
   return (
-    <>
+    <div style={{display: "flex", justifyContent: "space-around", padding: "25px"}}>
       <Assignment1 />
-    </>
+      <Assignment2 />
+    </div>
   )
 }
 
