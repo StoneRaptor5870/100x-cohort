21a41774975317071fd6824146ed8d7a89e47ae7
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-05 14:59:46 +0530

    user routes and controller

--- 10-week-8/wallet-backend/src/routes/userRouter.ts
+++ 10-week-8/wallet-backend/src/routes/userRouter.ts
@@ -1,3 +1,16 @@
 import { Router } from "express";
+import {
+  login,
+  logout,
+  searchBulk,
+  signup,
+} from "../controllers/userController";
+import { verifyToken } from "../middlewares/verifyToken";
 
 export const userRouter = Router();
+
+userRouter.post("/login", login);
+userRouter.post("/signup", signup);
+userRouter.post("/logout", logout);
+
+userRouter.get("/bulk", verifyToken, searchBulk);
