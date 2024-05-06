17824f5abf5144ecbe1fc7fe0053395e1c4a26b9
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-06 15:23:57 +0530

    store, header, userItem, theme and fixes

--- 10-week-8/wallet-frontend/src/App.tsx
+++ 10-week-8/wallet-frontend/src/App.tsx
@@ -1,14 +1,21 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import { Suspense } from "react";
+import { Toaster } from "sonner";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster position="bottom-right" />
+      <Header />
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </>
   )
 }
 
--- 10-week-8/wallet-frontend/src/main.tsx
+++ 10-week-8/wallet-frontend/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
