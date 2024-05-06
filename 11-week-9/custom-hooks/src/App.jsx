ddaf21a41cd1cae3cf9db6a7642dd38769fc2fd3
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-07 01:40:10 +0530

    browser functionality hooks

--- 11-week-9/custom-hooks/src/App.jsx
+++ 11-week-9/custom-hooks/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 // import AutoRefreshingHook from './hooks/AutoRefreshingHook'
 import DataFetchingHook from './hooks/DataFetchingHook'
+import UseIsOnlineHook from './hooks/UseIsOnlineHook'
+import UseMousePointerHook from './hooks/UseMousePointerHook'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <div className='todos'>
       <DataFetchingHook />
       {/* <AutoRefreshingHook /> */}
+      <UseIsOnlineHook />
+      <UseMousePointerHook />
     </div>
   )
 }
